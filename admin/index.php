bf56d0ee5f13aaa39d245bd1710763cd2e191a21
jananyi <janany0874@gmail.com>
2024-03-19 07:06:22 +0530

    newfeatures

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,52 @@
-<?php 
-    echo "Hello Admin";
-?>
+<html>
+    <head>
+        <title>Restaurant Website</title>
+    
+        <!-- Link our CSS file -->
+        <link rel="stylesheet" href="css/admin.css">
+    </head>
+    
+    <body>
+        <!-- Navbar Section Starts Here -->
+        <section class="navbar">
+            <div class="container">
+                <div class="logo">
+                    <a href="logo.html" title="Logo">
+                        <img src="images/logo.png" alt="Restaurant Logo" class="img-responsive">
+                    </a>
+                </div>
+    
+                <div class="menu text-right">
+                    <ul>
+                        <li>
+                            <a href="index.html">Home</a>
+                        </li>
+                        <li>
+                            <a href="categories.html">Categories</a>
+                        </li>
+                        <li>
+                            <a href="foods.html">Foods</a>
+                        </li>
+                        <li>
+                            <a href="contact.html">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+    
+                <div class="clearfix"></div>
+            </div>
+        </section>
+        <!-- Navbar Section Ends Here -->
+    
+            
+        <!-- footer Section Starts Here -->
+        <section class="footer">
+            <div class="container text-center">
+                <p>All rights reserved. Designed By <a href="#">VirtuiqEra</a></p>
+            </div>
+        </section>
+        <!-- footer Section Ends Here -->
+    
+    </body>
+    </html>
+
