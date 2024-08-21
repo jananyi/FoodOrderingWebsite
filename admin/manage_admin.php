7aceb4c56ae79cf8766679d7330886991e4b6511
jananyi <janany0874@gmail.com>
2024-08-21 23:17:38 +0530

    php commit

--- admin/manage_admin.php
+++ admin/manage_admin.php
@@ -3,6 +3,21 @@
         <div class="main-content">
             <div class="container">
                 <h1>Manage Admin</h1>
+                <table class="tbl_edit">
+                    <tr>
+                        <th>S.No</th>
+                        <th>Full Name</th>
+                        <th>User Name</th>
+                        <th>Actions</th>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Janany I</td>
+                        <td>Janany_I</td>
+                        <td>Update_Admin
+                            Delete_Admin</td>
+                    </tr>
+                </table>
             </div>
         </div>
         <!--Main Content Section Ends Here -->
